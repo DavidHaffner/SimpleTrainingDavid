d51b0555dbc80b9290cadf08ad15b6f12ea5676d
DavidHaffner <haffner@centrum.cz>
2020-07-22 13:04:02 +0200

    basic maven project structure

---
+++ src/main/java/cz/centrum/haffner/SimpleTrainingDavid/SimpleTrainingDavidApplication.java
@@ -0,0 +1,14 @@
+package cz.centrum.haffner.SimpleTrainingDavid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTrainingDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTrainingDavidApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/cz/centrum/haffner/SimpleTrainingDavid/SimpleTrainingDavidApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.centrum.haffner.SimpleTrainingDavid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTrainingDavidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
