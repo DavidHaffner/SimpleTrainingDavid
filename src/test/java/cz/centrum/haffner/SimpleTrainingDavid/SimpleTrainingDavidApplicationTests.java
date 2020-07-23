baaf28472cd765a58542f804d6b3f9c5ce189f71
DavidHaffner <haffner@centrum.cz>
2020-07-23 08:48:32 +0200

    zkouška pushe

--- src/test/java/cz/centrum/haffner/SimpleTrainingDavid/SimpleTrainingDavidApplicationTests.java
+++ src/test/java/cz/centrum/haffner/SimpleTrainingDavid/SimpleTrainingDavidApplicationTests.java
@@ -3,6 +3,7 @@ package cz.centrum.haffner.SimpleTrainingDavid;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class SimpleTrainingDavidApplicationTests {
 
